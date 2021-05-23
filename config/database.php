0233460380d64d641d434891eb6e2d81ad249ffd
0s4k4 <soulbosss@gmail.com>
2021-05-23 02:14:27 -0500

    creo la esctructura de carpetas para admin

---
+++ config/database.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+class Database{
+
+    private $host = 'localhost';
+    private $user = 'root';
+    private $password = '';
+    private $database = 'hotel';
+
+    private function getConnection()
+    {
+        $conn = new mysqli($this->host,$this->user, $this->password,$this->database);
+        if($conn->connect_error){
+            die("Error en la conexion a mysql: " . $conn->connect_error);
+        } else {
+            return $conn;
+        }
+    }
+}
+?>
---
+++ inc/container4.php
@@ -0,0 +1,19 @@
+<link rel="icon" href="https://www.phpzag.com/wp-content/uploads/2019/05/cropped-cropped-coollogo_com-7816453-60x60-32x32.png" sizes="32x32" />
+</head>
+<body class="">
+<nav class="navbar navbar-expand-md navbar-light" style="background-color: #73b2fc">
+    <a href="http://www.phpzag.com" class="navbar-brand">PHPZAG.COM</a>
+    <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+        <div class="navbar-nav">
+            <a href="http://www.phpzag.com" class="nav-item nav-link active">Home</a>            
+        </div>       
+    </div>
+</nav>
+
+<div class="container" style="min-height:500px;">
+
+	
